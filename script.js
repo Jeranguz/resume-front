968a2807cc6c31f68ef76edab123426641bf0833
Jeranguz <Jeremyu4231@gmail.com>
2026-03-17 16:07:37 -0600

    Add visitor counter functionality to the resume

---
+++ script.js
@@ -0,0 +1,18 @@
+const counterSpan = document.getElementById('counter');
+let counter = 0;
+
+const updateCounter = async () => {
+    response = await fetch('https://i61qu74qzh.execute-api.us-east-1.amazonaws.com/visitor')
+    .then(response => response.json())
+    .then(data => {
+        counter = data.body;
+        counterSpan.textContent = counter;
+    })
+    .catch(error => {
+        console.error('Error fetching visitor count:', error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateCounter();
+});
