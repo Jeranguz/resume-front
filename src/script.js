0636d748e381191f12603ea1fff48f2ca25c364d
Jeranguz <Jeremyu4231@gmail.com>
2026-03-23 16:29:43 -0600

    Update visitor API endpoint in script.js

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const counterSpan = document.getElementById('counter');
 let counter = 0;
 
 const updateCounter = async () => {
-    response = await fetch('https://i61qu74qzh.execute-api.us-east-1.amazonaws.com/visitor')
+    response = await fetch('https://krm5r2jro4.execute-api.us-east-1.amazonaws.com/visitor')
     .then(response => response.json())
     .then(data => {
         counter = data.body;
